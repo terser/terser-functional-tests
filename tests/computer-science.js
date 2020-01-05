001b5838fef014d1467d42e248b5da836c9f5993
Fábio Santos <fabiosantosart@gmail.com>
2020-01-05 17:25:33 +0000

    try to fix tests/computer-science.js

--- tests/computer-science.js
+++ tests/computer-science.js
@@ -3,14 +3,16 @@
 const assert = require('assert');
 const { readFileSync, writeFileSync } = require('fs');
 const { execSync } = require('child_process');
-const { minify } = require('terser');
+const { minify } = require(process.env.TERSER_PATH || 'terser');
 
 process.chdir(process.cwd() + '/computer-science');
 execSync('npm i');
 
 const code = readFileSync('../computer-science/index.js').toString();
 const minified = minify(code);
-assert(!minified.error)
+if (minified.error) {
+  throw minified.error
+}
 writeFileSync('../computer-science/index.min.js', minified.code)
 
 execSync('npm t', { stdio: ['pipe', process.stdout, process.stderr] });
